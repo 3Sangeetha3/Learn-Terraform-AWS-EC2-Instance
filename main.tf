256956ff5755a38d71b4da280316b8f0ce18ee5e
Jadamal Sangeetha Choudhary <jadamalsangeetha@lpu.in>
2025-07-01 12:41:24 +0530

    Creating multiple instances

--- main.tf
+++ main.tf
@@ -14,10 +14,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "my-first-server" {
+  #multiple instances
+  count = 4
   ami           = "ami-0d03cb826412c6b0f"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "terraform-aws-linux-example"
+    Name = "terraform-aws-linux-example-${count.index + 1}" 
+    # Using count.index to create unique names for each instance so that no conflict occurs
   }
 }
